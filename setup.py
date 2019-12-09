804380c62af98ff3e1382d51f104a25bd49077e8
Román Ramírez <rramirez@rootedcon.com>
2019-12-09 21:09:04 +0100

    Initial

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blueliv-patowc",
+    version="0.0.1",
+    author="Román Ramírez",
+    author_email="rramirez@rootedcon.com",
+    description="Blueliv's API encapsulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/patowc/blueliv",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
