c493c44dff869d3af7b4da0aebf849e0d4aad5f6
Román Ramírez <rramirez@rootedcon.com>
2019-12-10 15:31:22 +0100

    Moved User classes to users.py

--- api/core.py
+++ api/core.py
@@ -76,34 +76,3 @@ class BluelivRequest(BASERequestModel):
         else:
             raise Exception('[%s]: Exception with error code [%s]' % (url,
                                                                       str(r.status_code)))
-
-
-class BluelivUser(BASEModel):
-    user_id = None
-    username = None
-    first_name = None
-    last_name = None
-    karma = None
-    badge = None
-
-    def __init__(self, *args, **kwargs):
-        if 'user_id' in kwargs:
-            self.user_id = kwargs.get('user_id', None)
-
-        if 'username' in kwargs:
-            self.username = kwargs.get('username', None)
-
-        if 'first_name' in kwargs:
-            self.first_name = kwargs.get('first_name', None)
-
-        if 'last_name' in kwargs:
-            self.last_name = kwargs.get('last_name', None)
-
-        if 'karma' in kwargs:
-            self.karma = kwargs.get('karma', None)
-
-        if 'badge' in kwargs:
-            self.last_name = kwargs.get('badge', None)
-
-        super().__init__()
-
