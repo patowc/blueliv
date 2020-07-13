4e713493cac9a76d157530b3e924de0ae10856ad
Román Ramírez <rramirez@rootedcon.com>
2020-07-13 10:51:33 +0200

    Updated when refactoring api->blueliv

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 """
 This is the init module for the blueliv API package.
 
-The main package within is api, where all classes are located.
+The main package within is blueliv, where all classes are located.
 
-Version: 0.0.9
-Date: jun, 4 2020
+Version: 1.0.1
+Date: jul, 13 2020
 """
 
 
 __all__ = [
-    'api'
+    'blueliv'
 ]
