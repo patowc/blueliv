ec5b9ab586676cb01c1b3040d906b811e34c98c1
Román Ramírez <rramirez@rootedcon.com>
2020-07-14 08:51:40 +0200

    Version increment in docstring

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@ This is the init module for the blueliv API package.
 
 The main package within is blueliv, where all classes are located.
 
-Version: 1.0.1
-Date: jul, 13 2020
+Version: 1.0.2
+Date: jul, 14 2020
 """
 
 
